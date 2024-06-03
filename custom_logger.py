52396ee146ef81e6330b523f8b07ac9e56535540
Sarim Sikander <sarimsikander24@gmail.com>
2024-06-03 15:36:39 +0500

    Create custom_logger.py

---
+++ custom_logger.py
@@ -0,0 +1,22 @@
+import inspect
+import logging
+
+
+def customLogger(logLevel=logging.INFO):
+    # Gets the name of the class / method from where this method is called
+    loggerName = inspect.stack()[1][3]
+    logger = logging.getLogger(loggerName)
+    # By default, log all messages
+    logger.setLevel(logging.INFO)
+
+    fileHandler = logging.FileHandler("automation.log", mode="w")
+    fileHandler.setLevel(logLevel)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s: %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+    )
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+
+    return logger
